f9710242a025371821dcb3a103da431fd6b3a9d4
jobayer <jobayerrahman0220@gmail.com>
2025-09-24 20:39:02 +0600

    Design portfolio website UI

---
+++ src/pages/Index.tsx
@@ -0,0 +1,23 @@
+import HeroSection from "@/components/portfolio/HeroSection";
+import AboutSection from "@/components/portfolio/AboutSection";
+import PortfolioSection from "@/components/portfolio/PortfolioSection";
+import ExperienceSection from "@/components/portfolio/ExperienceSection";
+import TestimonialsSection from "@/components/portfolio/TestimonialsSection";
+import ContactSection from "@/components/portfolio/ContactSection";
+import Footer from "@/components/portfolio/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <HeroSection />
+      <AboutSection />
+      <PortfolioSection />
+      <ExperienceSection />
+      <TestimonialsSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
